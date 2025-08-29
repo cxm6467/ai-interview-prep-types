7ad7c026a36807bd74cffd25ec5cb38bfb6212c5
Chris Marasco <cxm6467@gmail.com>
2025-08-29 16:38:13 -0400

    test: add file without JSDoc to verify warnings work

---
+++ src/test-no-jsdoc.ts
@@ -0,0 +1,15 @@
+export function testFunction() {
+  return 'test';
+}
+
+export interface TestInterface {
+  name: string;
+}
+
+export type TestType = string | number;
+
+export class TestClass {
+  getName(): string {
+    return 'test';
+  }
+}
